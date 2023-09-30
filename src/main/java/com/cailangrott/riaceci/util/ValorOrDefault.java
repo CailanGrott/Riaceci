0d6e086a808ae05ddd63a0926a847f1d1abc9d4a
CailanGrott <cailangrott@gmail.com>
2023-09-30 13:10:42 -0300

    Removendo arquivos não utilizados

--- src/main/java/com/cailangrott/riaceci/util/ValorOrDefault.java
+++
@@ -1,7 +0,0 @@
-package com.cailangrott.riaceci.util;
-
-public class ValorOrDefault {
-    public static <T> T getValorOrDefault(T valorDefault, T value) {
-        return value == null || (value instanceof String && ((String) value).isBlank()) ? valorDefault : value;
-    }
-}
